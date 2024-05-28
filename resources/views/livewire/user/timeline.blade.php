b7b40cfa7a2fcb0cff8210c921624414c826aeb9
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2024-05-28 13:29:47 +0100

    changed the input to textbox

--- resources/views/livewire/user/timeline.blade.php
+++ resources/views/livewire/user/timeline.blade.php
@@ -1,4 +1,37 @@
 <div>
+    <style>
+        .form-control {
+            resize: none;
+            height: 100px; /* Adjust the height as needed */
+        }
+    
+        .stylish-button {
+            display: block;
+            width: 100%;
+            background-color: #007bff;
+            color: white;
+            padding: 10px 20px;
+            font-size: 16px;
+            transition: background-color 0.3s ease;
+            margin-top: 10px; /* Space between textarea and button */
+            text-align: center;
+        }
+    
+        .stylish-button:hover {
+            background-color: #0056b3;
+        }
+    
+        .stylish-button i {
+            margin-right: 5px;
+        }
+    
+        .btn-block {
+            display: block;
+            width: 100%;
+        }
+    </style>
+
+
     <div class="row">
         <div class="col-md-8">
             <!-- Post Update -->
@@ -11,16 +44,32 @@
         <div class="block block-bordered block-rounded">
             <div class="block-content block-content-full">
             <form wire:submit.prevent="post">
-            
-                <div class="input-group">
-                <input type="text" wire:model="content" name="content"  :value="old('content')" class="form-control form-control-alt @error('content') is-invalid @enderror" placeholder="Say something amazing" oninput="convertUrlsToLinks(this)" required>
-                
-                
-                <button type="submit" class="btn btn-primary border-0">
+                <div class="input-group mb-3">
+                    <textarea wire:model="content" name="content" class="form-control form-control-alt @error('content') is-invalid @enderror" placeholder="Say something amazing" required></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">
                     <i class="fa fa-pencil-alt opacity-50 me-1"></i> Post
                 </button>
 
+            
+                {{-- <div class="input-group">
+                <input type="text" wire:model="content" name="content"  :value="old('content')" class="form-control form-control-alt @error('content') is-invalid @enderror" placeholder="Say something amazing" oninput="convertUrlsToLinks(this)" required> 
+                
+                
+                 <button type="submit" class="btn btn-primary border-0">
+                    <i class="fa fa-pencil-alt opacity-50 me-1"></i> Post
+                </button>
+
+
+                <div class="input-group mb-3">
+                    <textarea wire:model="content" name="content" class="form-control form-control-alt @error('content') is-invalid @enderror" placeholder="Say something amazing" required></textarea>
+                    
+                    <button type="submit" class="btn btn-primary border-0 stylish-button">
+                        <i class="fa fa-pencil-alt opacity-50 me-1"></i> Post
+                    </button>
                 </div>
+
+                </div> --}}
             </form>
             </div>
         </div>
