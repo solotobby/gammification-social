74dd60b9ef32b1010693a0d1baddd1bd5b7f91ba
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2024-06-08 19:01:23 +0100

    fixed error

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -20,10 +20,9 @@
 
     <!-- Icons -->
     <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
-    <link rel="shortcut icon" href="{{ asset('src/assets/media/favicons/favicon.png') }}">
-    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('src/assets/media/favicons/favicon-192x192.png') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('src/assets/media/favicons/apple-touch-icon-180x180.png') }}">
-    <!-- END Icons -->
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicon.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon.png') }}">    <!-- END Icons -->
 
     <!-- Stylesheets -->
     <!-- Page JS Plugins CSS -->
