38af528a03446482d9c7ae1fb0a955253db3014a
Oluwatobi Solomon <solomon.oluwatobi@edutech.global>
2024-06-19 22:26:27 +0100

    webhook implementation

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Partner;
+use App\Models\PartnerSlot;
+use App\Models\Transaction;
 use App\Models\Webhook;
 use Illuminate\Http\Request;
 
@@ -10,8 +13,71 @@ class WebhookController extends Controller
     public function handle(Request $request){
 
         $event = $request["event"];
-
         Webhook::create(['event' => $event, 'content' => $request]);
+
+        if($event == 'charge.completed'){
+
+            $transactionRef = $request['data']['tx_ref']; //get tx ref from webhook
+
+            //validate transaction
+            $transaction = Transaction::where('ref', $transactionRef)->first();
+
+            $string = $transaction->meta;
+            $data = json_decode($string, true);
+            $package = htmlspecialchars($data['package']);
+            $slotNumber = htmlspecialchars($data['number_of_slot']);
+
+            //get per info
+            $partnerId = @$transaction->user->partner->id;
+            $partner = PartnerSlot::find($partnerId);//where('partner_id', $partnerId)->first();
+            // $partner = Partner::where('user_id', $transaction->user_id)->first();
+
+            if($partner->status == true){
+                if($package == 'Influencer'){
+                    $partner->influencer += $slotNumber;
+                    $partner->save();
+
+                    $transaction->status = 'allocated';
+                    $transaction->save();
+
+                    return response()->json(['status' => 'success']);
+    
+                    // $this->updateTx($request->tx_ref);
+    
+                    // return back()->with('success', 'Influencer slot Updated successfully');
+    
+                } elseif($package == 'Creator'){
+                    $partner->creator += $slotNumber;
+                    $partner->save();
+
+                    $transaction->status = 'allocated';
+                    $transaction->save();
+
+                    return response()->json(['status' => 'success']);
+    
+                    // $this->updateTx($request->tx_ref);
+    
+                    // return back()->with('success', 'Creator slot Updated successfully');
+                }else{
+    
+                    $partner->beginner += $slotNumber;
+                    $partner->save();
+
+                    $transaction->status = 'allocated';
+                    $transaction->save();
+
+                    return response()->json(['status' => 'success']);
+    
+                    // $this->updateTx($request->tx_ref);
+    
+                    // return back()->with('success', 'Beginner slot Updated successfully');
+    
+                }
+            }
+
+
+            
+        }
 
 
     }
