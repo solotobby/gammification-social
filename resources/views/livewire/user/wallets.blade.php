5ebe2cc996c31d31335c88741434e6305137f96d
Oluwatobi Solomon <solotobby@gmail.com>
2026-02-01 15:26:15 +0000

    fdbfg

--- resources/views/livewire/user/wallets.blade.php
+++ resources/views/livewire/user/wallets.blade.php
@@ -169,7 +169,7 @@
                                 <div class="ms-2 me-auto">
                                     <div class="fw-bold">Payout on Engagement</div>
 
-                                    {{ number_format(@$payouts->amount ?? 'Pending') }}
+                                    {{ number_format(@$payouts->amount) ?? 'Pending' }}
 
                                     {{-- <i>Payment will be updated on the
                                        <b> {{ Carbon\Carbon::now()->addMonth()->day(1)->format('F j, Y') }}</b></i> --}}
