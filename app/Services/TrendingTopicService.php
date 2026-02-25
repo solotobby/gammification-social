35e9cba135f4a9c66cef44592376788ce23d955f
Oluwatobi Solomon <solotobby@gmail.com>
2026-02-25 09:34:08 +0000

    deploy

--- app/Services/TrendingTopicService.php
+++ app/Services/TrendingTopicService.php
@@ -95,34 +95,158 @@ class TrendingTopicService
 
     private function extractPhrases(string $text): array
     {
-        // 1️⃣ Lowercase everything
+        // 1️⃣ Lowercase
         $text = strtolower($text);
 
-        // 2️⃣ Remove URLs / anything that looks like a URL
-        $text = preg_replace('/https?:\/\/\S+|www\.\S+|\S+\.(com|net|org|io|gov|edu|ng|co\.\w+)/', '', $text);
+        // 2️⃣ Remove URLs (http, https, www, domain-like strings)
+        $text = preg_replace('/https?:\/\/\S+|www\.\S+|\b\S+\.(com|net|org|io|gov|edu|ng|co\.\w+)\b/', '', $text);
 
-        // 3️⃣ Remove non-word characters except hashtags
+        // 3️⃣ Remove punctuation except hashtags
         $text = preg_replace('/[^\w\s#]/', '', $text);
 
         // 4️⃣ Split into words
         $words = array_values(array_filter(explode(' ', $text)));
 
+        // 5️⃣ Stop words list (expand as needed)
+        $stopWords = [
+            'the',
+            'and',
+            'for',
+            'are',
+            'but',
+            'not',
+            'you',
+            'all',
+            'any',
+            'can',
+            'had',
+            'her',
+            'was',
+            'one',
+            'our',
+            'out',
+            'day',
+            'get',
+            'has',
+            'him',
+            'his',
+            'how',
+            'man',
+            'new',
+            'now',
+            'old',
+            'see',
+            'two',
+            'way',
+            'who',
+            'boy',
+            'did',
+            'its',
+            'let',
+            'put',
+            'say',
+            'she',
+            'too',
+            'use',
+            'they',
+            'we',
+            'what',
+            'when',
+            'where',
+            'why',
+            'i',
+            'me',
+            'my',
+            'mine',
+            'he',
+            'his',
+            'she',
+            'her',
+            'it',
+            'its',
+            'to',
+            'of',
+            'in',
+            'your'
+        ];
+
+        // 6️⃣ Filter words: remove short words & stop words
+        $filteredWords = array_values(array_filter($words, function ($word) use ($stopWords) {
+
+            // Keep hashtags regardless of length
+            if (str_starts_with($word, '#')) {
+                return true;
+            }
+
+            // Remove words less than 3 characters
+            if (strlen($word) < 3) {
+                return false;
+            }
+
+            // Remove stop words
+            if (in_array($word, $stopWords)) {
+                return false;
+            }
+
+            // Remove numeric-only words
+            if (ctype_digit($word)) {
+                return false;
+            }
+
+            return true;
+        }));
+
         $phrases = [];
 
-        // 5️⃣ Extract hashtags
+        // 7️⃣ Extract hashtags separately
         preg_match_all('/#\w+/', $text, $hashtags);
         $phrases = array_merge($phrases, $hashtags[0]);
 
-        // 6️⃣ Bigrams
-        for ($i = 0; $i < count($words) - 1; $i++) {
-            $phrases[] = $words[$i] . ' ' . $words[$i + 1];
+        // 8️⃣ Bigrams
+        for ($i = 0; $i < count($filteredWords) - 1; $i++) {
+            $phrases[] = $filteredWords[$i] . ' ' . $filteredWords[$i + 1];
         }
 
-        // 7️⃣ Trigrams
-        for ($i = 0; $i < count($words) - 2; $i++) {
-            $phrases[] = $words[$i] . ' ' . $words[$i + 1] . ' ' . $words[$i + 2];
+        // 9️⃣ Trigrams
+        for ($i = 0; $i < count($filteredWords) - 2; $i++) {
+            $phrases[] = $filteredWords[$i] . ' ' . $filteredWords[$i + 1] . ' ' . $filteredWords[$i + 2];
         }
 
-        return $phrases;
+        return array_unique($phrases);
     }
+
+
+
+    // private function extractPhrases(string $text): array
+    // {
+    //     // 1️⃣ Lowercase everything
+    //     $text = strtolower($text);
+
+    //     // 2️⃣ Remove URLs / anything that looks like a URL
+    //     $text = preg_replace('/https?:\/\/\S+|www\.\S+|\S+\.(com|net|org|io|gov|edu|ng|co\.\w+)/', '', $text);
+
+    //     // 3️⃣ Remove non-word characters except hashtags
+    //     $text = preg_replace('/[^\w\s#]/', '', $text);
+
+    //     // 4️⃣ Split into words
+    //     $words = array_values(array_filter(explode(' ', $text)));
+
+    //     $phrases = [];
+
+    //     // 5️⃣ Extract hashtags
+    //     preg_match_all('/#\w+/', $text, $hashtags);
+    //     $phrases = array_merge($phrases, $hashtags[0]);
+
+    //     // 6️⃣ Bigrams
+    //     for ($i = 0; $i < count($words) - 1; $i++) {
+    //         $phrases[] = $words[$i] . ' ' . $words[$i + 1];
+    //     }
+
+    //     // 7️⃣ Trigrams
+    //     for ($i = 0; $i < count($words) - 2; $i++) {
+    //         $phrases[] = $words[$i] . ' ' . $words[$i + 1] . ' ' . $words[$i + 2];
+    //     }
+
+    //     return $phrases;
+    // }
 }
