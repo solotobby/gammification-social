d73cc6c9e573801f234aadd0ecfef9d61254f9fb
Oluwatobi Solomon <solotobby@gmail.com>
2026-04-02 16:36:14 +0100

    fghfdgh

--- resources/views/livewire/user/timeline.blade.php
+++ resources/views/livewire/user/timeline.blade.php
@@ -1,3 +1,40 @@
 <div>
-    testing page
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/glightbox/dist/js/glightbox.min.js"></script>
+    
+    <style>
+        .form-control {
+            resize: none;
+
+            /* Adjust the height as needed */
+        }
+
+        .stylish-button {
+            display: block;
+            width: 100%;
+            background-color: #007bff;
+            color: white;
+            padding: 10px 20px;
+            font-size: 16px;
+            transition: background-color 0.3s ease;
+            margin-top: 10px;
+            /* Space between textarea and button */
+            text-align: center;
+        }
+
+        .stylish-button:hover {
+            background-color: #0056b3;
+        }
+
+        .stylish-button i {
+            margin-right: 5px;
+        }
+
+        .btn-block {
+            display: block;
+            width: 100%;
+        }
+    </style>
+    <?php $userLevel = userLevel(); ?>
+
 </div>
