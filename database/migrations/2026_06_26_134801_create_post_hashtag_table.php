11cf651de08b7b39d1c2bac728f93ec7af35a1b1
Oluwatobi Solomon <solotobby@gmail.com>
2026-06-26 21:21:55 +0100

    updated migration

--- database/migrations/2026_06_26_134801_create_post_hashtag_table.php
+++ database/migrations/2026_06_26_134801_create_post_hashtag_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('post_hashtag', function (Blueprint $table) {
-            // $table->uuid('id')->primary();
+            $table->uuid('id')->primary();
             $table->uuid('post_id');
                 // ->constrained()
                 // ->cascadeOnDelete();
