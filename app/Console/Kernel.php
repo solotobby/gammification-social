cd523f82e2a2cd431e744dc90b45fd6d57358820
Oluwatobi Solomon <solotobby@gmail.com>
2026-07-06 17:30:46 +0100

    deploy it

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -276,9 +276,9 @@ class Kernel extends ConsoleKernel
                 ));
         })->dailyAt('00:40');
 
-        $schedule->call(function () {
-            app(TrendingTopicService::class)->genenrateTrendingTopics();
-        })->everyThirtyMinutes();
+        // $schedule->call(function () {
+        //     app(TrendingTopicService::class)->genenrateTrendingTopics();
+        // })->everyThirtyMinutes();
 
         $schedule->call(function () {
 
@@ -299,21 +299,21 @@ class Kernel extends ConsoleKernel
             });
         })->dailyAt('23:00');
 
-        // $schedule->call(function () {
-        //     $subject = 'Daily Engagement Registered - updated';
-        //     $content = "Registered Daily Stats successfully";
+        $schedule->call(function () {
+            $subject = 'Test Mail';
+            $content = "Registered Daily Stats successfully";
 
 
-        //     Mail::to('solotob3@gmail.com')
-        //         ->send(new GeneralMail(
-        //             (object)[
-        //                 'name' => 'Oluwatobi Solomon',
-        //                 'email' => 'solotob3@gmail.com'
-        //             ],
-        //             $subject,
-        //             $content
-        //         ));
-        // })->hourly();
+            Mail::to('solotob3@gmail.com')
+                ->send(new GeneralMail(
+                    (object)[
+                        'name' => 'Oluwatobi Solomon',
+                        'email' => 'solotob3@gmail.com'
+                    ],
+                    $subject,
+                    $content
+                ));
+        })->everyFiveMinutes();
     }
 
     /**
