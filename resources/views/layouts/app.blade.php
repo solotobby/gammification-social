5ac1c0a67caba37109182ac9c1b7f2615a703e3b
Oluwatobi Solomon <solotobby@gmail.com>
2026-09-01 23:26:29 +0100

    cool

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,22 +44,6 @@
     <link rel="stylesheet" id="css-main" href="{{ asset('src/assets/css/dashmix.css') }}">
     {{-- <link rel="stylesheet" id="css-theme" href="{{ asset('src/assets/css/themes/xmodern.min.css') }}"> --}}
 
-
-    <script type="text/javascript">
-        var Tawk_API = Tawk_API || {},
-            Tawk_LoadStart = new Date();
-        (function() {
-            var s1 = document.createElement("script"),
-                s0 = document.getElementsByTagName("script")[0];
-            s1.async = true;
-            s1.src = 'https://embed.tawk.to/661c41d4a0c6737bd12bb507/1hrf629ii';
-            s1.charset = 'UTF-8';
-            s1.setAttribute('crossorigin', '*');
-            s0.parentNode.insertBefore(s1, s0);
-        })();
-    </script>
-    <!--End of Tawk.to Script-->
-
     {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"> --}}
 
     <style>
